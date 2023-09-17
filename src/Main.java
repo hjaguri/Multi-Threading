0b6e88c1828481101e246a3fe60b8711ea620bf8
Hansraj Jaguri <hjaguri27@gmail.com>
2023-09-17 18:15:41 +0530

    Multithreading file

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+
+/* Two way to create a thread
+1. By using Thread class
+2. By using the runnable interface
+ */
+
+public class Main {
+    public static void main(String[] args) {
+myThread mt=new myThread();//Now create thread object
+        mt.start(); // now we can call run method using start method call (invoke the thread).
+        //if we can call again the start method then they thorw the error.We can can not invoke the thread exception
+
+ myRunnable mr=new myRunnable();
+ Thread th=new Thread(mr);
+ th.start();
+
+    }
+}
+
+
+/* Now first create thread by using thread class
+1.first extends the thread class in your class
+2. then you can create run method
+ */
+class myThread extends Thread{
+    public void run(){
+        System.out.println("Threading");
+    }
+}
+
+class myRunnable implements Runnable{
+        public void run(){
+            System.out.println("Runnable");
+        }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------
+
---
+++ src/SecondLecture.java
@@ -0,0 +1,47 @@
+// Different cases of executing Thread
+// There mainly four way
+//1. Performing  Single task from single thread
+//2.Performing Single task from Multiple Thread
+//3.Performing Multiple task from Single thread  ---> This is not possible because if you run single thread in multiple task  then the whole program is  corrpect
+//4.Performing Multiple task  from  Multiple thread
+
+
+public class SecondLecture {
+    public static void main(String []args){ // main thread is also a thread which is create by JVm
+                Test t=new Test();
+                t.start(); // single task from single thread
+
+        Test t1=new Test();
+        t1.start(); //  t and t2 Single task from Multiple Thread
+
+
+        //----------------------------------------------------------------------------------------
+        myThread mt1=new myThread();
+        mt1.start();
+        myThread2 mt2=new myThread2(); // Multiple task  from  Multiple thread
+        mt2.start();
+
+    }
+}
+
+
+//1. Performing  Single task from single thread
+class Test extends Thread{
+    public void run(){
+        System.out.println("Thread task");
+    }
+}
+//---------------------------------------------------------------------------------------------------------
+   // 4 Performing Multiple task  from  Multiple thread
+
+class myThread1 extends Thread{
+    public void run(){
+        System.out.println("Task 1");
+    }
+}
+
+class myThread2 extends Thread{
+    public void run(){
+        System.out.println("Task 2 ");
+    }
+}
